7a6237280b13518aa21ce87561ac2562e9329cf5
Kundan <kundanmahato34@gmail.com>
2022-12-08 10:04:30 -0800

    added forget password link call

--- src/Components/Authentication/AuthForm.js
+++ src/Components/Authentication/AuthForm.js
@@ -5,12 +5,13 @@ import AuthContext from "../Store/auth-context";
 import { useContext } from "react";
 
 const AuthForm = () => {
-   const authCntxt =  useContext(AuthContext)
+  const authCntxt = useContext(AuthContext);
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
-  
+  const [resetMail, setresetMail] = useState('');
+  const [isforgetform, setforgetform] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -29,46 +30,81 @@ const AuthForm = () => {
       url =
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyANtNuRfZ4qD9R-7t5p05uN9TjqzPbj93o";
     } else {
-      url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyANtNuRfZ4qD9R-7t5p05uN9TjqzPbj93o"
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyANtNuRfZ4qD9R-7t5p05uN9TjqzPbj93o";
     }
-      fetch(url,
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }).then(async(res) => {
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(async (res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = "Authentication failed!";
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
 
-          if (res.ok) {
-            return res.json();
-          } else {
-            return res.json().then((data) => {
-              let errorMessage = 'Authentication failed!';
-              // if (data && data.error && data.error.message) {
-              //   errorMessage = data.error.message;
-              // }
-  
-              throw new Error(errorMessage);
-            });
-          }
-        })
-        .then((data) => {
-          authCntxt.login(data.idToken);
-          console.log(data.idToken)
-        })
-        .catch((err) => {
-          alert(err.message);
-        });
-        console.log('user login succesfully kundan')
-    };
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        authCntxt.login(data.idToken);
+        console.log(data.idToken);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+    console.log("user login succesfully kundan");
+  };
+  const ForgetformHandler=(e)=>{
+    setforgetform((current) => !current);
+  }
+  async function PasswordResetmailHandler() {
+    const response = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyANtNuRfZ4qD9R-7t5p05uN9TjqzPbj93o",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType:"PASSWORD_RESET",
+          email:resetMail,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+
+      console.log('kundan555');
+  }
+  const resetMailHandler =(e)=>{
+    e.preventDefault();
+    setresetMail(e.target.value)
+    }
+
+
+
+
+
+
+
+
+
 
   return (
     <section className={classes.auth}>
+      {!isforgetform && (<>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
@@ -84,6 +120,9 @@ const AuthForm = () => {
             ref={passwordInputRef}
           />
         </div>
+        <div >
+          <button onClick={ForgetformHandler}>forget password</button>
+        </div>
         <div className={classes.actions}>
           <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
@@ -95,6 +134,20 @@ const AuthForm = () => {
           </button>
         </div>
       </form>
+ </>)}
+      {isforgetform &&(
+        <div>
+          <h4 className={classes.control}>Enter the mail which u have entered</h4>
+          <div className={classes.control}>
+          <label htmlFor="email">Registered Email</label>
+          <input type="email" id="email" value={resetMail} onChange={resetMailHandler} required  />
+        </div>
+        <div className={classes.actions}>
+          <button onClick={PasswordResetmailHandler}>send link</button>
+          <button onClick={ForgetformHandler}>already a user Sign in</button>
+        </div>
+        </div>
+      )}
     </section>
   );
 };
